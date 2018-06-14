a43692bcdc3c9c239dc62aa3bf1fab8d5a6bee05
Prashant Chaurasia <prashant.chaurasia1311@gmail.com>
2018-06-14 11:30:31 +0530

    Initial Commit data fetched from newsapi.org and displayed

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,21 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.controller('newsCtrl',newsCtrl);
+
+function newsCtrl($scope,$http){
+  $scope.news = []
+  $http({
+    method: "GET",
+    url: "https://newsapi.org/v2/top-headlines?country=in&apiKey=2afd7b026354408f8b13ae1ff7a5b836"
+  }).then(function (newsData) {
+    $scope.news = newsData.data['articles'];
+  })
+}
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
